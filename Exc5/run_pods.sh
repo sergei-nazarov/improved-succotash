1e539b8950a1fa686bca4a6334a3d635a646a2c3
sergey <mamban1997@gmail.com>
2024-11-13 00:42:42 +0500

    fixes

--- Exc5/run_pods.sh
+++ Exc5/run_pods.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-kubectl run front-end-app --image=nginx --labels role=front-end --expose --port 80
-kubectl run back-end-api-app --image=nginx --labels role=back-end-api --expose --port=80
-kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --expose --port=80
-kubectl run admin-back-end-api-app --image=nginx --labels role=admin-back-end-api --expose --port=80
+kubectl run front-end-app --image=nginx --labels role=front-end --port=80
+kubectl run back-end-api-app --image=nginx --labels role=back-end-api --port=80
+kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --port=80
+kubectl run admin-back-end-api-app --image=nginx --labels role=admin-back-end-api --port=80
+
+kubectl apply -f front-end-service.yaml
+kubectl apply -f back-end-api-service.yaml
+kubectl apply -f admin-front-end-service.yaml
+kubectl apply -f admin-back-end-api-service.yaml
